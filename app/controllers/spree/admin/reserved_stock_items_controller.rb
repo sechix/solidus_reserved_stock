58d7e5501a7e6d5e7a262d71d7e7810989689af1
sechix <sechix@gmail.com>
2020-07-07 13:42:11 +0200

    Update reserved_stock_items_controller.rb

--- app/controllers/spree/admin/reserved_stock_items_controller.rb
+++ app/controllers/spree/admin/reserved_stock_items_controller.rb
@@ -40,7 +40,7 @@ module Spree
       def restock
         reserved_stock_item = Spree::ReservedStockItem.find(params[:id])
         variant = reserved_stock_item.variant
-        Spree::Stock::Reserver.new(variant, @user, reserved_stock_item.original_stock_location).restock(variant, @user)
+        Spree::Stock::Reserver.new(variant, @user, reserved_stock_item.original_stock_location).restock
         flash[:success] = flash_message_for(@reserved_stock_item, :successfully_restocked)
         redirect_to admin_user_reserved_stock_items_path(@user)
       rescue => e
